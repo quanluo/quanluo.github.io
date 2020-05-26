e2a7cef4aca24c0841f026783ddf80b10bfe6f8d
Unmasking <53830661+JosenTong@users.noreply.github.com>
2020-05-26 17:05:26 +0800

    Site updated: 2020-05-26 17:05:21

--- dist/musicPlay.js
+++ dist/musicPlay.js
@@ -2,63 +2,55 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     // mini: false,
     fixed: true,//吸底模式
-    autoplay: false,//默认是否自动播放
+    autoplay: true,//默认是否自动播放
     theme: '#FADFA3',
     loop: 'all',
     order: 'random',
     preload: 'auto',
-    volume: 0.7,//默认音量
+    volume: 0.5,//默认音量
     mutex: true,//是否不允许多个播放器同时播放
     listFolded: true,//是否默认收起播放列表
     listMaxHeight: 90,//播放列表的高度
-    lrcType: 3, //使用lrc文件提供歌词
+    //lrcType: 3, //使用lrc文件提供歌词
 	//网易云默认外链链接：http://music.163.com/song/media/outer/url?id=ID数字.mp3
     audio: [
         {
-            name: 'SURPRISE-DRIVE',
-            artist: '松冈充',
-            url: 'http://music.163.com/song/media/outer/url?id=29751409.mp3',
-            cover: 'http://p1.music.126.net/Oo0kXusaoF9DGwjAJFeAtA==/6671836558494023.jpg',
-            lrc: '/./dist/music/lrc/29751409.lrc',
+            name: '夏の喚く',
+            artist: '邱有句',
+            url: 'http://music.163.com/song/media/outer/url?id=434871406.mp3',
+            cover: 'http://p1.music.126.net/7BPYzlYTQp285WXdO-u2Ug==/18215609137826245.jpg',
+            lrc: '/./dist/music/lrc/434871406.lrc',
             theme: '#46718b'
         },
         {
-            name: 'Be The One',
-            artist: 'PANDORA / Beverly',
-            url: 'http://music.163.com/song/media/outer/url?id=530986958.mp3',
-            cover: 'http://p1.music.126.net/91YBBcxEO37hoIfm9W5TjA==/109951163116520306.jpg',
+            name: 'Journey',
+            artist: 'Capo Productions',
+            url: 'http://music.163.com/song/media/outer/url?id=16846088.mp3',
+            cover: 'http://p1.music.126.net/x-LVbj4tX6oEDH3qkqJP2g==/663005511556827.jpg',
             theme: '#46718b'
         },
 		{
-            name: '我ら思う、故に我ら在り',
-            artist: '氣志團',
-            url: 'http://music.163.com/song/media/outer/url?id=40729281.mp3',
-            cover: 'http://p1.music.126.net/5kUo3f5_GyyQNiSvPOIhXA==/18610333813518047.jpg',
+            name: 'River Flows In You',
+            artist: 'Martin Ermen',
+            url: 'http://music.163.com/song/media/outer/url?id=20110049.mp3',
+            cover: 'http://p1.music.126.net/QnFSKGqltSSQ1PAO3z44dg==/5680077069158217.jpg',
             theme: '#46718b'
         },
 		{
-            name: 'Anything Goes! (「仮面ライダーオーズ/000」)',
-            artist: '大黒摩季',
-            url: 'http://music.163.com/song/media/outer/url?id=38689229.mp3',
-            cover: 'http://p2.music.126.net/mQxtTZ1t2ZHS8LJ4qEDLWQ==/2543170398297999.jpg',
+            name: 'Somewhere',
+            artist: 'July',
+            url: 'http://music.163.com/song/media/outer/url?id=5307932.mp3',
+            cover: 'http://p1.music.126.net/wpk6nSJhIhmQTzCq7oh_Hg==/731175232477827.jpg',
             lrc: '/./dist/music/lrc/38689229.lrc',
 	    theme: '#46718b'
         },
 		{
-            name: 'Break the Chain (「仮面ライダーキバ」)',
-            artist: 'Tourbillon',
-            url: 'http://music.163.com/song/media/outer/url?id=38689234.mp3',
-            cover: 'http://p2.music.126.net/mQxtTZ1t2ZHS8LJ4qEDLWQ==/2543170398297999.jpg',
+            name: '盗将行',
+            artist: '花粥 / 马雨阳',
+            url: 'http://music.163.com/song/media/outer/url?id=574566207.mp3',
+            cover: 'http://p2.music.126.net/-qHPT3rhxDlu5zQV9NcQ-A==/109951163555860423.jpg',
             lrc: '/./dist/music/lrc/38689234.lrc',
 	    theme: '#46718b'
         },
-		{
-            name: 'Just Live More',
-            artist: '湘南乃風',
-            url: 'http://rh01.sycdn.kuwo.cn/resource/n2/99/25/305360002.mp3',
-            cover: 'http://img1.kuwo.cn/star/starheads/120/82/85/3360615613.jpg',
-	    lrc: '/./dist/music/lrc/湘南乃風-Just Live More.lrc',
-            theme: '#46718b'
-        }
     ]
 });
